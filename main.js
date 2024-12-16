5411c612ab120399ab980164cf5df7603b6c65b6
Elham <elhamwaheed777@gmail.com>
2024-12-16 17:14:55 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// Map
+let arr = [1, 2, 3, 4];
+arr.map((val) => {
+  console.log(val);
+});
+// 
+const newArr = arr.map((val)=>{
+    return val * val
+})
+console.log(newArr); // Return new Arr
+
+
